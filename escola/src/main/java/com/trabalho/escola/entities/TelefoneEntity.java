0246ac862258cc1229b9c4037d62308a107deaa8
Gabriel Damico <gabrieldamico22@gmail.com>
2023-05-15 21:03:50 -0300

    Criei a Entidade Telefone

---
+++ escola/src/main/java/com/trabalho/escola/entities/TelefoneEntity.java
@@ -0,0 +1,37 @@
+package com.trabalho.escola.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_telefone")
+public class TelefoneEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "numero")
+	private Integer numero;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	
+	
+}
