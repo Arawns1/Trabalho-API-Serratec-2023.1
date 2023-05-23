c19f3118bf6a00ec6921512c00a8afddbc4a05cb
Gabriel Damico <gabrieldamico22@gmail.com>
2023-05-22 21:10:44 -0300

    Trabalho 02

---
+++ src/main/java/com/trabalho/escola/controllers/TurmaController.java
@@ -0,0 +1,60 @@
+package com.trabalho.escola.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trabalho.escola.entities.Turma;
+import com.trabalho.escola.services.TurmaService;
+
+@RestController
+@RequestMapping("/turmas")
+public class TurmaController {
+
+	@Autowired
+	TurmaService turmaService;
+	
+	@GetMapping
+	public ResponseEntity<List<Turma>> GetAllTurmas() {
+		return new ResponseEntity<>(turmaService.GetAllTurmas(),
+					HttpStatus.OK);
+ }
+	@GetMapping("/{id}")
+	public ResponseEntity<Turma> getTurmaById (@PathVariable Integer id) {
+		Turma turmaResponse = turmaService.getTurmaById(id);
+		if(null == turmaResponse)
+			return new ResponseEntity<>(null,
+					HttpStatus.NOT_FOUND);
+		else
+			return new ResponseEntity<>(turmaResponse,
+					HttpStatus.OK);
+		}
+	@PostMapping
+	public ResponseEntity<Turma> saveTurma (@RequestBody Turma turma) {
+		return new ResponseEntity<>(turmaService.saveTurma(turma),
+				HttpStatus.CREATED);
+	}
+	@PutMapping
+	public  ResponseEntity<Turma> UpdateEditora (@RequestBody Turma turma, Integer id) {
+		return new ResponseEntity<>(turmaService.UpdateTurma(turma, id),
+				HttpStatus.OK);
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> delTurma(@PathVariable Integer id) {
+		Boolean resp = turmaService.delTurma(id);
+		if (resp)
+			return new ResponseEntity<>(resp, HttpStatus.OK);
+		else
+			return new ResponseEntity<>(resp, HttpStatus.NOT_MODIFIED);
+	}
+}
