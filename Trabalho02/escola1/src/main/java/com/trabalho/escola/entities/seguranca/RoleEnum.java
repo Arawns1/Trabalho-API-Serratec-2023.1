1444bbd0b9554d3aeba9a0bed6269f670d9c5027
Gabriel Damico <gabrieldamico22@gmail.com>
2023-05-22 22:20:42 -0300

    Trabalho dois movido e enviando email

---
+++ Trabalho02/escola1/src/main/java/com/trabalho/escola/entities/seguranca/RoleEnum.java
@@ -0,0 +1,8 @@
+package com.trabalho.escola.entities.seguranca;
+
+public enum RoleEnum {
+	ROLE_USER,
+	ROLE_MODERATOR,
+	ROLE_ADMIN,
+	ROLE_CONTROLADOR
+}
---
+++ Trabalho02/escola1/src/main/java/com/trabalho/escola/repositories/seguranca/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.trabalho.escola.repositories.seguranca;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.trabalho.escola.entities.seguranca.Role;
+import com.trabalho.escola.entities.seguranca.RoleEnum;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+	Optional<Role> findByName(RoleEnum name);
+}
